a6f1ea3e8c1dac9ac8344c8fe6ccd35cb8c9cf2d
Nathan Pham <nathanpham.me@gmail.com>
2021-03-26 11:42:11 -0700

    frame rate

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ pygame.init()
 
 WINDOW_SIZE = (400, 400)
 WINDOW_NAME = "My First Pygame Window"
+FRAME_RATE = 60
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 pygame.display.set_caption(WINDOW_NAME)
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -16,3 +18,4 @@ while True:
             sys.exit()
 
     pygame.display.update()
+    clock.tick(FRAME_RATE)
